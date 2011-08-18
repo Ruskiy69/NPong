4736f04a67f0b4c7da959df2f6f5167c77b31545
George Kudrayvtsev <zloi.tatarin@yahoo.com>
2011-08-18 14:06:08 -0700

    Comments and changelog

--- Source/NPong.cpp
+++ Source/NPong.cpp
@@ -1,3 +1,21 @@
+/****************************************************
+*              NPong (Networked Pong)               *
+*                                                   *
+*****************************************************
+*   Author  : George Kudrayvtsev (patience)         *
+*   OS      : Windows                               *
+*   Depends : SDL, SDL_image, SDL_ttf, SDL_mixer    *
+*   Tested  : Windows 7 64-Bit                      *
+*   Version : 0.9.4 BETA                            *
+*                                                   *
+*   Description:                                    *
+*      The classic arcade game of pong, with a      *
+*      nearly unbeatable AI, and the ability        *
+*      to play against another human on your        *
+*      network.                                     *
+*                                                   *    
+****************************************************/
+
 #include "Engine.h"
 
 int main(int argc, char* argv[])
